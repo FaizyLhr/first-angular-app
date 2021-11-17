f28f5362073e9daa650de08a7b60d89477003d74
Faizy <FaizyLhr>
2021-11-17 20:40:42 +0500

    Upadted Manual Geneated Component

--- src/app/MyComponents/man-ang-compo/man-ang-compo.component.ts
+++ src/app/MyComponents/man-ang-compo/man-ang-compo.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-man-ang-compo',
-  template: `<h1>
-      Worldddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd!
-    </h1>
+  template: `<h1>World!!</h1>
     <p>This template definition spans multiple lines.</p> `,
-  //   templateUrl: './man-ang-compo.component.html',
-  //   styleUrls: ['./man-ang-compo.component.css'],
+  // templateUrl: './man-ang-compo.component.html',
+  // styleUrls: ['./man-ang-compo.component.css'],
   styles: ['h1 { background-color: grey; }'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class manAngCompoComponent {}
