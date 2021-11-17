1a3449f7e719bbc246c51a76a5279a321521bf09
Faizy <FaizyLhr>
2021-11-17 20:42:28 +0500

    Learned Encapsulation

---
+++ src/app/MyComponents/sec-man-gen-compo/sec-man-gen-compo.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-sec-man-gen-compo',
+  templateUrl: './sec-man-gen-compo.component.html',
+  styleUrls: ['./sec-man-gen-compo.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom,
+})
+export class secManGenCompo {}
