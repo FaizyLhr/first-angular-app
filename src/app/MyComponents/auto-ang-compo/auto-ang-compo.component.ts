51f26417d62ce271cccf6f93a57fb1ad10146899
Faizy <FaizyLhr>
2021-11-17 20:41:08 +0500

    Updated Automatic Generated Component

--- src/app/MyComponents/auto-ang-compo/auto-ang-compo.component.ts
+++ src/app/MyComponents/auto-ang-compo/auto-ang-compo.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-auto-ang-compo',
   templateUrl: './auto-ang-compo.component.html',
-  styleUrls: ['./auto-ang-compo.component.css']
+  styleUrls: ['./auto-ang-compo.component.css'],
+  encapsulation: ViewEncapsulation.Emulated, //Default
 })
 export class AutoAngCompoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
