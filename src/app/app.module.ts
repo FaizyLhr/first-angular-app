d1e492dd954ede5576bb7627b1a444e5b8493e45
Faizy <FaizyLhr>
2021-11-17 20:42:42 +0500

    Learned directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AutoAngCompoComponent } from './MyComponents/auto-ang-compo/auto-ang-compo.component';
 import { manAngCompoComponent } from './MyComponents/man-ang-compo/man-ang-compo.component';
+import { secManGenCompo } from './MyComponents/sec-man-gen-compo/sec-man-gen-compo.component';
+import { ParentChildComponent } from './MyComponents/parent-child/parent-child.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [AppComponent, AutoAngCompoComponent, manAngCompoComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    AutoAngCompoComponent,
+    manAngCompoComponent,
+    secManGenCompo,
+    ParentChildComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
