9cf1afabd66b9adb12a48dc0426bcdbf25da32eb
Faizy <FaizyLhr>
2021-11-19 15:46:44 +0500

    One Way Binding Done

---
+++ src/app/MyComponents/binding/binding.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-binding',
+  templateUrl: './binding.component.html',
+  styleUrls: ['./binding.component.css'],
+})
+export class BindingComponent implements OnInit {
+  val: string = 'Init';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
