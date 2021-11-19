862278c62a7c7e02561af6e5d09bd5b85a4677b4
Faizy <FaizyLhr>
2021-11-19 20:21:28 +0500

    Learned Setter And getter

--- src/app/MyComponents/binding/binding.component.ts
+++ src/app/MyComponents/binding/binding.component.ts
@@ -7,16 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BindingComponent implements OnInit {
   val: string = 'Init';
-
   source: string = '../../../assets/Images/Faizy.jpg';
-
   bgColor: string = 'green';
-
   fontSize: number = 30;
-
   bolVal: boolean = false;
-
   uName: string = 'Faizy';
+  price: number = 45;
+
+  // Getter and Setter
+  private _list: string = '';
+
+  get list(): string {
+    console.log('In Getter : ', this._list);
+    return this._list;
+  }
+  set list(value: string) {
+    this._list = value;
+    console.log('In Setter : ', value);
+  }
 
   toggleBoolean(): void {
     this.bolVal = !this.bolVal;
@@ -24,5 +32,7 @@ export class BindingComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.list = 'Faizy';
+  }
 }
